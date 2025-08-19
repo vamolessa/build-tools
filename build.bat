80f530480a19383a717ff0a3f5dbf014dedb2438
vamolessa <1696072+vamolessa@users.noreply.github.com>
2025-08-19 17:12:36 -0300

    wip

--- build.bat
+++ build.bat
@@ -19,9 +19,7 @@ if exist "%ProgramFiles%\7-Zip\7z.exe" (
 rem ===========================================================================
 
 git clone https://git.sr.ht/~lessa/absolute-unit || exit /b 1
-dir .
-dir absolute-unit
-cd absolute-unit
+cd "absolute-unit"
 call build.bat || exit /b 1
 cd ..
 
