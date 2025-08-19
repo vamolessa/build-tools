cbf42f2c3b5c0aa68fa658f5382a93a71ac88287
vamolessa <1696072+vamolessa@users.noreply.github.com>
2025-08-19 17:14:07 -0300

    wip

--- build.bat
+++ build.bat
@@ -18,8 +18,8 @@ if exist "%ProgramFiles%\7-Zip\7z.exe" (
 
 rem ===========================================================================
 
-git clone https://git.sr.ht/~lessa/absolute-unit || exit /b 1
-cd "absolute-unit"
+git clone https://git.sr.ht/~lessa/absolute-unit au || exit /b 1
+cd au
 call build.bat || exit /b 1
 cd ..
 
@@ -27,14 +27,14 @@ git clone https://git.sr.ht/~lessa/peppito || exit /b 1
 cd peppito
 git submodule set-url foundation https://git.sr.ht/~lessa/foundation
 git submodule update --init --recursive
-..\absolute-unit\au --optimize || exit /b 1
+..\au\au --optimize || exit /b 1
 cd ..
 
 rem ===========================================================================
 
 mkdir tools-win
 
-copy /y absolute-unit\au.exe tools-win
+copy /y au\au.exe tools-win
 copy /y peppito\build\peppito.exe tools-win
 
 if "%GITHUB_WORKFLOW%" neq "" (
