fc15b4f8bb0b0cf8bc6a4be2be0a10771cdd566b
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2025-10-20 01:05:09 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,16 +6,11 @@ rm -f tools
 mkdir tools
 pushd tools
 
-git clone https://git.sr.ht/~lessa/absolute-unit
-pushd absolute-unit
-./build.sh || exit 1
-popd
-
 git clone https://git.sr.ht/~lessa/peppito
 pushd peppito
 git submodule set-url foundation https://git.sr.ht/~lessa/foundation
 git submodule update --init --recursive
-../absolute-unit/au -c clang --release || exit 1
+./build.sh -c clang --release || exit 1
 popd
 
 popd
@@ -24,7 +19,6 @@ popd
 
 mkdir tools-$OS
 
-cp tools/absolute-unit/au tools-$OS
 cp tools/peppito/build/peppito tools-$OS
 
 rm -f tools-$OS.zip
