75518b45d0547ca4172cd67f53d6db9dd02e2cc2
vamolessa <1696072+vamolessa@users.noreply.github.com>
2026-09-20 00:50:09 -0300

    wtf

--- build.bat
+++ build.bat
@@ -23,8 +23,7 @@ call git clone https://git.sr.ht/~lessa/peppito
 pushd peppito
 call git submodule set-url foundation https://git.sr.ht/~lessa/foundation
 call git submodule update --init --recursive
-call foundation\bootstrap.bat
-call tcl.bat --release build.tcl || echo exit /b 1
+call tcl.bat --release build.tcl || exit /b 1
 popd
 
 rem call git clone https://git.sr.ht/~lessa/lsp
